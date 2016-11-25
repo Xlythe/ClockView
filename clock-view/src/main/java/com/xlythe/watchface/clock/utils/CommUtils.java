76dc92f250ca31f0e188a30839287f128fefdc20
Will <xlythe@gmail.com>
2016-11-24 21:25:12 -0800

    temp workaround for bug in Play Services

--- clock-view/src/main/java/com/xlythe/watchface/clock/utils/CommUtils.java
+++ clock-view/src/main/java/com/xlythe/watchface/clock/utils/CommUtils.java
@@ -1,7 +1,11 @@
 package com.xlythe.watchface.clock.utils;
 
+import android.app.Activity;
+import android.app.Application;
 import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.AnyThread;
@@ -12,6 +16,7 @@ import android.util.Log;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.DataEvent;
 import com.google.android.gms.wearable.DataEventBuffer;
 import com.google.android.gms.wearable.DataItem;
@@ -111,17 +116,39 @@ public class CommUtils {
      */
     @AnyThread
     public static void put(Context context, final String key, final String value) {
-        final GoogleApiClient client = new GoogleApiClient.Builder(context.getApplicationContext()).addApi(Wearable.API).build();
+        GoogleApiClient client = new GoogleApiClient.Builder(context.getApplicationContext())
+                .addApi(Wearable.API)
+                .build();
+        put(context, client, key, value, true /* isFirstRun */);
+    }
+
+    /**
+     * Put a message that can be read on all devices
+     */
+    @AnyThread
+    private static void put(final Context context, final GoogleApiClient client, final String key, final String value, final boolean isFirstRun) {
+        final BugfixHelper bugfixHelper = isFirstRun ? new BugfixHelper(context, client) : null;
         sExecutorService.submit(new Runnable() {
             @Override
             public void run() {
-                ConnectionResult result = client.blockingConnect(TIMEOUT, TimeUnit.MILLISECONDS);
-                if (!result.isSuccess()) {
-                    Log.w(TAG, "Failed to connect to GoogleApiClient: [" + result.getErrorCode() + "]" + result.getErrorMessage());
-                    return;
+                if (!client.isConnected()) {
+                    ConnectionResult result = client.blockingConnect(TIMEOUT, TimeUnit.MILLISECONDS);
+                    if (!result.isSuccess()) {
+                        Log.w(TAG, "Failed to connect to GoogleApiClient: [" + result.getErrorCode() + "]" + result.getErrorMessage());
+                        return;
+                    }
                 }
+
+                if (bugfixHelper != null) {
+                    bugfixHelper.onConnected(key, value);
+                }
+
                 put(client, key, value);
-                client.disconnect();
+
+                // Only disconnect if the bugfix helper isn't running
+                if (bugfixHelper != null) {
+                    client.disconnect();
+                }
             }
         });
     }
@@ -213,5 +240,114 @@ public class CommUtils {
 
     public interface Callback {
         void onCallback(String result);
+    }
+
+    private static class ActivityLifecycleCallbacks implements Application.ActivityLifecycleCallbacks {
+        @Override
+        public void onActivityCreated(Activity activity, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onActivityStarted(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityResumed(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityPaused(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityStopped(Activity activity) {
+        }
+
+        @Override
+        public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onActivityDestroyed(Activity activity) {
+
+        }
+    }
+
+    /**
+     * Due to a bug in GMSCore, put does not always work.
+     * We'll listen for changes and, if we don't hear any, we'll try again.
+     */
+    private static class BugfixHelper {
+        private static final long BUGFIX_DELAY = 300;
+
+        private final Context mContext;
+        private final GoogleApiClient mClient;
+
+        private final Handler mHandler = new Handler(Looper.getMainLooper());
+        private final DataApi.DataListener mListener;
+
+        @Nullable
+        private final Activity mActivity;
+        @Nullable
+        private final Application mApplication;
+        @Nullable
+        private final ActivityLifecycleCallbacks mActivityLifecycleCallbacks;
+
+        BugfixHelper(Context context, final GoogleApiClient client) {
+            mContext = context;
+            mClient = client;
+            mListener = new DataApi.DataListener() {
+                @Override
+                public void onDataChanged(DataEventBuffer dataEventBuffer) {
+                    cleanup(true /* disconnect */);
+                }
+            };
+
+            if (context instanceof Activity) {
+                mActivity = (Activity) context;
+                mApplication = mActivity.getApplication();
+                mActivityLifecycleCallbacks = new ActivityLifecycleCallbacks() {
+                    @Override
+                    public void onActivityStopped(Activity activity) {
+                        if (mActivity == activity) {
+                            cleanup(true /* disconnect */);
+                        }
+                    }
+                };
+                mApplication.registerActivityLifecycleCallbacks(mActivityLifecycleCallbacks);
+            } else {
+                mActivity = null;
+                mApplication = null;
+                mActivityLifecycleCallbacks = null;
+            }
+        }
+
+        void onConnected(final String key, final String value) {
+            Wearable.DataApi.addListener(mClient, mListener);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    put(mContext, mClient, key, null, false /* isFirstRun */);
+                    put(mContext, mClient, key, value, false /* isFirstRun */);
+                    cleanup(false /* disconnect */);
+                }
+            }, BUGFIX_DELAY);
+        }
+
+        private void cleanup(boolean disconnect) {
+            mHandler.removeCallbacksAndMessages(null);
+            Wearable.DataApi.removeListener(mClient, mListener);
+            if (mApplication != null) {
+                mApplication.unregisterActivityLifecycleCallbacks(mActivityLifecycleCallbacks);
+            }
+            if (disconnect) {
+                mClient.disconnect();
+            }
+        }
     }
 }
